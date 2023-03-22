83f2b71faee726261dd1fedc1bb1163ae56748d5
Snehasrg <sghongade642@gmail.com>
2023-03-23 00:33:37 +0530

    Method calling

--- Program.cs
+++ Program.cs
@@ -6,8 +6,40 @@ namespace Pattern
     {
         public static void Main(string[] args)
         {
-             SumofAllDigit.CalculateSum();
+            Console.WriteLine("Please enter your choice form below:");
+            Console.WriteLine("\n1.DuplicateElementinArray\n2.UniqueElement\n3.FrequencyofElement\n4.MaxMinElement\n5.ReverseStar\n6.HollowSquarePattern\n7.ReverseLetterofString\n8.SumofAllDigit\n9.SumofEachRowofMatrix\n");
+            int ch=Convert.ToInt32(Console.ReadLine());
 
+             switch (ch)
+             {
+                    case 1:
+                    DuplicateElementinArray.CheckDuplicate();
+                    break;
+                    case 2:
+                    UniqueElement.UniqueIntElement();
+                    break;
+                    case 3:            
+                    //FrequencyofElement.Checkfrequency();
+                    break; 
+                    case 4:
+                    MaxMinElement.FindMaxMin();
+                    break;
+                    case 5:
+                    ReverseStar.DisplayStar();
+                    break;
+                    case 6:
+                    HollowSquarePattern.PrintPattern();
+                    break;
+                    case 7:
+                    HollowSquarePattern.PrintPattern();
+                    break;
+                    case 8:
+                    SumofAllDigit.CalculateSum();
+                    break;
+                    case 9:
+                    SumofEachRowofMatrix.DisplayMatrix();
+                    break;
+             }
         }
     }
 }
